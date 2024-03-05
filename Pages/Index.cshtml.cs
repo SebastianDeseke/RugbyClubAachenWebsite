bdc0421239f3c2e6bd85f4080e07edac366a97f6
Sebastian Deseke <deseke97@gmail.com>
2024-03-05 16:54:54 +0100

    Added dynamic language object

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -12,8 +12,29 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    public object getLanguage () {
+        string acceptLanguage = Request.Headers["Accept-Language"];
+        string[] languages = acceptLanguage.Split(',');
+
+        for (int i = 0; i < languages.Length; i++)
+        {
+            if (languages[0].Contains("*")){
+                ViewData["Language"] = "en";
+                continue;
+            } else if (languages[i].Contains("de")) {
+                ViewData["Language"] = "de";
+                break;
+            } else if (languages[i].Contains("en")) {
+                ViewData["Language"] = "en";
+                break;
+            }
+        }
+
+        return ViewData["Language"];
+    }
+
     public void OnGet()
     {
-
+        getLanguage();
     }
 }
