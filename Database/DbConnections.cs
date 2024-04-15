da3379390b09bc122d65b34c7635358968129e5e
Sebastian Deseke <deseke97@gmail.com>
2024-04-15 16:23:23 +0200

    Added UserFetcher and gave more body to PictureFetcher

--- Database/DbConnections.cs
+++ Database/DbConnections.cs
@@ -25,34 +25,98 @@ public class DbConnections
         connection.Close();
     }
 
-    /*
-    public void CreateCustomer(string KundenId, string Title, string Vorname, string Nachname, string Firmenname, string Adresse, string Postleizahl, string Telefonnummer, string Email, string Umsatzsteuernummer, int preisKategorie){
-    string sqlQuery = @"INSERT INTO 
-    kundendaten (KundenID, AnsprechTitle, AnsprechVorname, AnsprechNachname, Firmenname, Adresse, Postleizahl, Telefonnummer, Email, Umsatzsteuernummer, PreisKategorie)
-    VALUES (@Value0, @Value1, @Value2, @Value3, @Value4, @Value5, @Value6, @Value7, @Value8, @Value9, @Value10)";
 
-    // Set up a command object with your SQL query and connection
-    Connect();
-    MySqlCommand command = new MySqlCommand(sqlQuery, connection);
+    public void CreateUser(string UID, string username, string name, string surname, string Email, string create_time, string password)
+    {
+        string sqlQuery = @"INSERT INTO 
+        users (UID, username, name, surname, Email, create_time, password)
+        VALUES (@Value0, @Value1, @Value2, @Value3, @Value4, @Value5, @Value6)";
 
-    // Set the parameter values for your query
-    command.Parameters.AddWithValue("@Value0", KundenId);
-    command.Parameters.AddWithValue("@Value1", Title);
-    command.Parameters.AddWithValue("@Value2", Vorname);
-    command.Parameters.AddWithValue("@Value3", Nachname);
-    command.Parameters.AddWithValue("@Value4", Firmenname);
-    command.Parameters.AddWithValue("@Value5", Adresse);
-    command.Parameters.AddWithValue("@Value6", Postleizahl);
-    command.Parameters.AddWithValue("@Value7", Telefonnummer);
-    command.Parameters.AddWithValue("@Value8", Email);
-    command.Parameters.AddWithValue("@Value9", Umsatzsteuernummer);
-    command.Parameters.AddWithValue("@Value10", preisKategorie);
+        // Set up a command object with your SQL query and connection
+        Connect();
+        MySqlCommand command = new MySqlCommand(sqlQuery, connection);
 
-    // Execute the query
-    command.ExecuteNonQuery();
-    command.Dispose();
-    Disconnect();
-}*/
+        // Set the parameter values for your query
+        command.Parameters.AddWithValue("@Value0", UID);
+        command.Parameters.AddWithValue("@Value1", username);
+        command.Parameters.AddWithValue("@Value2", name);
+        command.Parameters.AddWithValue("@Value3", surname);
+        command.Parameters.AddWithValue("@Value4", Email);
+        command.Parameters.AddWithValue("@Value5", create_time);
+        command.Parameters.AddWithValue("@Value6", password);
 
+        // Execute the query
+        command.ExecuteNonQuery();
+        command.Dispose();
+        Disconnect();
+    }
+
+    public void EditUser(string UID, string username, string UpdateInput, string UpdateValue)
+    {
+        string sqlQuery = @$"UPDATE users
+        SET {UpdateInput} = {UpdateValue}
+        WHERE UID = @UID";
+
+        Connect();
+        MySqlCommand command = new MySqlCommand(sqlQuery, connection);
+
+        command.ExecuteNonQuery();
+        command.Dispose();
+        Disconnect();
+
+    }
+
+    public void DeleteUser(string UID)
+    {
+        string sqlQuery = @"DELETE FROM users
+        WHERE UID = @UID";
+
+        Connect();
+        MySqlCommand command = new MySqlCommand(sqlQuery, connection);
+
+        command.ExecuteNonQuery();
+        command.Dispose();
+        Disconnect();
+
+    }
+
+    public void GetUser(string UID)
+    {
+        string sqlQuery = @"SELECT * FROM users
+        WHERE UID = @UID";
+
+        Connect();
+        MySqlCommand command = new MySqlCommand(sqlQuery, connection);
+
+        command.ExecuteNonQuery();
+        command.Dispose();
+        Disconnect();
+
+    }
+
+    public void UploadForm(){
+
+    }
+
+        public void CreatePicture() {
+
+    }
+
+    public void EditPicture() {
+        //edit the information of the picture, like for example the alt text or author
+
+    }
+
+    public void DeletePicture() {
+
+    }
+
+    public void GetAllPictures() {
+        //get all the pictures from the database
+
+    }
+
+    public void FetchPicturesCarousel() {
+
+    }
 }
-
--- Fetchers/PictureFetcher.cs
+++ Fetchers/PictureFetcher.cs
@@ -1,13 +1,25 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
+using RugbyClubAachenWeb.Database;
 
-namespace RugbyClubAachenWeb.Fetchers
-{
+namespace RugbyClubAachenWeb.Fetchers;
+
     public class PictureFetcher
     {
-        public void GetPictures()
+        public string Picture { get; set; }
+        public string[] Pictures { get; set; }
+        DbConnections db = new DbConnections();
+
+        public void GetAllPictures()
         {
-            Console.WriteLine("Pictures are loaded");
+            db.GetAllPictures();
+            Console.WriteLine("All pictures are loaded");
         }
+
+        public void GetSinglePicture (string PictureID)
+        {
+            db.GetSinglePicture(PictureID);
+            Console.WriteLine("Picture is loaded");
+        }
+
     }
-}
