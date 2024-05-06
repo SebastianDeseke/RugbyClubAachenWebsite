7dcfe81ec9bbe48a137b6a1f70c9ac3aa60ea54d
Sebastian Deseke <deseke97@gmail.com>
2024-05-06 16:54:35 +0200

    improvements to the images process

--- Fetchers/PictureFetcher.cs
+++ Fetchers/PictureFetcher.cs
@@ -4,41 +4,43 @@ using RugbyClubAachenWeb.Database;
 
 namespace RugbyClubAachenWeb.Fetchers;
 
-    public class PictureFetcher
+public class PictureFetcher
+{
+    public string Picture { get; set; }
+    public string[] Pictures { get; set; }
+    private readonly DbConnections _db;
+    private readonly IConfiguration _config;
+
+    public PictureFetcher(DbConnections db, IConfiguration config)
     {
-        public string Picture { get; set; }
-        public string[] Pictures { get; set; }
-        private readonly DbConnections _db;
-        private readonly IConfiguration _config;
-
-        public PictureFetcher(DbConnections db, IConfiguration config)
-        {
-            _db = db;
-            _config = config;
-        }
-
-        public void GetAllPictures(int amount)
-        {
-            if (amount >= 500 || amount <= 0)
-            {
-                Console.WriteLine("Invalid amount of pictures requested");
-                return;
-            }
-            _db.GetAllPictures(amount);
-            Console.WriteLine("All pictures are loaded");
-        }
-
-        public void GetSinglePicture (string PictureID)
-        {
-            _db.GetSinglePicture(PictureID);
-            Console.WriteLine("Picture is loaded");
-        }
-
-        public void showPictureCarousel(int i)
-        {
-            string[] tmp = _db.FetchPicturesCarousel();
-            return tmp[i];
-            Console.WriteLine("Carousel pictures are loaded");
-        }
-
+        _db = db;
+        _config = config;
     }
+
+    public void GetAllPictures(int amount)
+    {
+        if (amount >= 500 || amount <= 0)
+        {
+            Console.WriteLine("Invalid amount of pictures requested");
+            return;
+        }
+        _db.GetAllPictures(amount);
+        Console.WriteLine("All pictures are loaded");
+    }
+
+    public void GetSinglePicture(string PictureID)
+    {
+        _db.GetSinglePicture(PictureID);
+        Console.WriteLine("Picture is loaded");
+    }
+
+    public string showPictureCarousel(int i)
+    {
+        //string[] tmp = _db.FetchPicturesCarousel();
+        string[] tmp2 = new string[5] { "~/images/IMG-20240314-WA0001.jpg", "~/images/IMG-20240314-WA0002.jpg", "~/images/IMG-20240314-WA0003.jpg", "~/images/IMG-20240314-WA0004.jpg", "~/images/IMG-20240314-WA0005.jpg" };
+        Console.WriteLine("Carousel pictures are loaded");
+        //return tmp[i];
+        return tmp2[i];
+    }
+
+}
